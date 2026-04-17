75e61aa03d9301b3a4cf6d73e788510639aa6f10
Jérôme Wiedemann <jerome@wnetworks.org>
2026-04-17 20:30:49 +0200

    Update

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 HA_VERSION=$(grep homeassistant requirements.txt | cut -d '=' -f 3)
 
-docker build --build-arg HA_VERSION="$HA_VERSION" -t RomRider/hass-custom-devcontainer:"$HA_VERSION" .
+docker buildx build --platform linux/amd64,linux/arm64 --build-arg HA_VERSION="$HA_VERSION" -t romrider/hass-custom-devcontainer:"$HA_VERSION" -t romrider/hass-custom-devcontainer:latest .
